dafda75e5c2fa045669c8efd1d53f854acfc0045
Ankit6055 <ankit.kumarrrrr6055@gmail.com>
2025-05-21 13:57:58 +0530

    Auth Done

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,14 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
+app.use(
+    cors({
+      origin: "http://localhost:5174",
+      credentials: true,
+    })
+  );
 app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -16,6 +26,6 @@ app.get("/", (req, res) => {
 
 app.use("/api/v1/auth", authRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
--- Backend/src/middleware/auth.middleware.js
+++ Backend/src/middleware/auth.middleware.js
@@ -1,118 +1,74 @@
 import jwt from "jsonwebtoken";
-import {db} from "../libs/db.js"
-
-// export const authMiddleware = async (req , res , next)=>{
-//     try {
-//         console.log(req.cookies.token);
-//         const token = req.cookies.jwt;
-//         console.log("Token:", token);
-
-//         if(!token){
-//             return res.status(401).json({
-//                 message:"Unauthorized - No token provided"
-//             })
-//         }
-
-//         let decoded;
-
-//         try {
-//             decoded = jwt.verify(token , process.env.JWT_SECRET);
-//         } catch (error) {
-//             return res.status(401).json({
-//                 message:"Unauthorized - Invalid token"
-//             })
-//         }
-
-//         const user = await db.user.findUnique({
-//             where:{
-//                 id:decoded.id
-//             },
-//             select:{
-//                 id:true,
-//                 image:true,
-//                 name:true,
-//                 email:true,
-//                 role:true
-//             }
-//         });
-
-
-//         if(!user){
-//             return res.status(404).json({message:"User not found"});
-//         }
-
-//         req.user = user;
-//         next();
-
-//     } catch (error) {
-//         console.error("Error authenticating user:", error);
-//         res.status(500).json({message:"Error authenticating user"});
-//     }
-// }
+import { db } from "../libs/db.js";
 
 export const authMiddleware = async (req, res, next) => {
-      try {
-            const token = req.cookies.token;
-            if (!token) {
-                  throw new ApiError(401, "Unauthorized - No token found");
-            };
+  try {
+    console.log(req.cookies);
+    const token = req.cookies.jwt;
 
-            let decoded;
+    if (!token) {
+      return res.status(401).json({
+        message: "Unauthorized - No token provided",
+      });
+    }
 
-            try {
-                  decoded = jwt.verify(token, process.env.JWT_SECRET);
-            } catch (error) {
-                  throw new ApiError(401, "Unauthorized - Invalid token");
-            };
+    let decoded;
 
-            const user = await db.user.findUnique({
-                  where: {
-                        id: decoded.id
-                  },
-                  select: {
-                        id: true,
-                        name: true,
-                        username: true,
-                        email: true,
-                        avatar: true,
-                        role: true
-                  }
-            });
+    try {
+      decoded = jwt.verify(token, process.env.JWT_SECRET);
+    } catch (error) {
+      return res.status(401).json({
+        message: "Unauthorized - Invalid token",
+      });
+    }
 
-            if (!user) {
-                  throw new ApiError(401, "Unauthorized - User not found");
-            };
+    const user = await db.user.findUnique({
+      where: {
+        id: decoded.id,
+      },
+      select: {
+        id: true,
+        image: true,
+        name: true,
+        email: true,
+        role: true,
+      },
+    });
 
-            req.user = user;
-            next();
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
 
-      } catch (error) {
-            throw new ApiError(401, error?.message || "Error authenticating user");
-      }
+    req.user = user;
+    next();
+  } catch (error) {
+    console.error("Error authenticating user:", error);
+    res.status(500).json({ message: "Error authenticating user" });
+  }
 };
 
-export const checkAdmin  = async(req , res , next)=>{
-    try {
-        const userId = req.user.id;
-        
-        const user = await db.user.findUnique({
-            where:{
-                id:userId
-            },
-            select:{
-                role:true
-            }
-        })
+export const checkAdmin = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
 
-        if(!user || user.role !== "ADMIN"){
-            return res.status(403).json({
-                message:"Access denied - Admins only"
-            })
-        }
+    const user = await db.user.findUnique({
+      where: {
+        id: userId,
+      },
+      select: {
+        role: true,
+      },
+    });
 
-        next();
-    } catch (error) {
-        console.error("Error checking admin role:", error);
-        res.status(500).json({message:"Error checking admin role"});
+    if (!user || user.role !== "ADMIN") {
+      return res.status(403).json({
+        message: "Access denied - Admins only",
+      });
     }
-}
+
+    next();
+  } catch (error) {
+    console.error("Error checking admin role:", error);
+    res.status(500).json({ message: "Error checking admin role" });
+  }
+};
