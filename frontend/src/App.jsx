1b1dff31e9f0ecbd64e687458cdd053a85ae75d8
Ankit6055 <ankit.kumarrrrr6055@gmail.com>
2025-05-24 02:15:41 +0530

    Setup Vite Project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./page/HomePage";
+import SignUpPage from "./page/SignUpPage";
+import LoginPage from "./page/LoginPage";
+
+const App = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
